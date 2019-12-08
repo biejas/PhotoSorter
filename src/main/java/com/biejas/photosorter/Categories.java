dd13f742372b39d37088b75d6eedda0b046369dc
biekin <kinga.kb@interia.pl>
2019-12-08 16:40:26 +0100

    Add Categories

---
+++ src/main/java/com/biejas/photosorter/Categories.java
@@ -0,0 +1,11 @@
+package main.java.com.biejas.photosorter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class Categories {
+    private static final Map<String, Category> registry = new HashMap<>();
+
+    private Categories() {
+    }
+}
