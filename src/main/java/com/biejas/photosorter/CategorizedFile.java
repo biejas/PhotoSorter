76225877da48ff7b7098312f24acec7d7402273d
biekin <kinga.kb@interia.pl>
2019-12-09 11:44:57 +0100

    Connect gui with sorter

--- src/main/java/com/biejas/photosorter/CategorizedFile.java
+++ src/main/java/com/biejas/photosorter/CategorizedFile.java
@@ -13,7 +13,8 @@ import java.util.Map;
 
 public class CategorizedFile {
     private File file;
-    private List<Category> categories;
+    private Category topCategory;
+    private List<Category> categories = new ArrayList<>();
 
     public CategorizedFile(File file) {
         this.file = file;
@@ -24,46 +25,16 @@ public class CategorizedFile {
         return this;
     }
 
-    public void categorize(Integer threshold){
-        System.out.println("Categorizing file " + file.getName() + " ...");
-        try (ImageAnnotatorClient vision = ImageAnnotatorClient.create()) {
+    public void setTopCategory(){
+        topCategory = categories.get(0);
+    }
 
-            // The path to the image file to annotate
-            String fileName = file.getCanonicalPath();
+    public File getFile() {
+        return file;
+    }
 
-            // Reads the image file into memory
-            Path path = Paths.get(fileName);
-            byte[] data = Files.readAllBytes(path);
-            ByteString imgBytes = ByteString.copyFrom(data);
-
-            // Builds the image annotation request
-            List<AnnotateImageRequest> requests = new ArrayList<>();
-            Image img = Image.newBuilder().setContent(imgBytes).build();
-            Feature feat = Feature.newBuilder().setType(Feature.Type.LABEL_DETECTION).build();
-            AnnotateImageRequest request = AnnotateImageRequest.newBuilder()
-                    .addFeatures(feat)
-                    .setImage(img)
-                    .build();
-            requests.add(request);
-
-            // Performs label detection on the image file
-            BatchAnnotateImagesResponse response = vision.batchAnnotateImages(requests);
-            List<AnnotateImageResponse> responses = response.getResponsesList();
-
-            for (AnnotateImageResponse res : responses) {
-                if (res.hasError()) {
-                    System.out.printf("Error: %s\n", res.getError().getMessage());
-                    return;
-                }
-
-                for (EntityAnnotation annotation : res.getLabelAnnotationsList()) {
-                    annotation.getAllFields().forEach((k, v) ->
-                            System.out.printf("%s : %s\n", k, v.toString()));
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public Category getTopCategory() {
+        return topCategory;
     }
 }
 
